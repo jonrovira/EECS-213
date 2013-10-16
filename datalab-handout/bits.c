166ccb3a7d23b58df7d88cca1668661a53322c2f
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2013-10-16 01:17:42 -0500

    updated bits.c

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -140,8 +140,23 @@ NOTES:
  *   Rating: 4
  */
 int absVal(int x) {
-  return 2;
+       /*
+	 Gets sign bit and takes the complement.
+	 This gives an int of all 1's and an int
+	 of all 0's. Exploiting ~x + 1 gives the
+	 negative of x. Exploiting & with x and (~x+1)
+	 for oppSignExt and ~oppSignExt generates 
+	 the absolute value.
+	*/
+       int sign = x >> 31;
+       int oppSignExt = ~sign;
+       int q = oppSignExt & x;
+       int r = ~oppSignExt & (~x + 1);
+
+       return q | r;
 }
+
+
 /* 
  * bang - Compute !x without using !
  *   Examples: bang(3) = 0, bang(0) = 1
@@ -150,9 +165,26 @@ int absVal(int x) {
  *   Rating: 4 
  */
 int bang(int x) {
-  return 2;
+  /*
+    Generates all 1's if x is negative or if x + TMAX is negative.
+    If either of these is all 1's, | give all 1's, and ~ gives
+    all 0's. If x is 0, adding TMAX leaves the sign bit 0,
+    so right shifting gives all 0's still. So | gives all 0's, ~ 
+    gives all 1's. Taking & 0x1 gives 1 for 0 and 0 for anything 
+    else. 
+   */
+
+  int neg = x >> 31;
+
+  int pos = (x + ~(0x1 << 31)) >> 31;
+
+  int ans = ~(neg | pos) &  0x1;
+
+  return ans;
 
 }
+
+
 /*
  * bitParity - returns 1 if x contains an odd number of 0's
  *   Examples: bitParity(5) = 0, bitParity(7) = 1
@@ -163,6 +195,8 @@ int bang(int x) {
 int bitParity(int x) {
   return 2;
 }
+
+
 /* 
  * byteSwap - swaps the nth byte and the mth byte
  *  Examples: byteSwap(0x12345678, 1, 3) = 0x56341278
@@ -172,7 +206,23 @@ int bitParity(int x) {
  *  Max ops: 25
  *  Rating: 2
  */
-int byteSwap(int x, int n, int m) {
+int byteSwap(int x, int n, int m) { 
+  /*
+    Exploits left shifting by 3 to multiply both m and n by 8.  
+    Left shift a byte of all 1's (0xFF) by the shifted m
+    and another by the shifted n; the & operation separates
+    the mth and nth bytes from x, stored separately.
+
+    Right shifting and left shifting by the opposite values 
+    switches the m and n positions.
+
+    Using XOR and ~ the nonswitched bytes are selected and
+    the & stores these bytes. 
+
+    Finally, adding the not switched bytes with the ^ of the
+    switched bytes yields the result with bytes m and n swapped.
+   */
+
   int result;
   int FF = 0xFF;
 
@@ -190,9 +240,8 @@ int byteSwap(int x, int n, int m) {
   byteM = byteM >> mShifted; //1
   byteN = byteN >> nShifted; //1
 
-  byteM = byteM & FF;
-  byteN = byteN & FF;
-
+  byteM = byteM & FF; //1
+  byteN = byteN & FF; //1
   byteM = byteM << nShifted; //1
   byteN = byteN << mShifted; //1
 
@@ -200,7 +249,11 @@ int byteSwap(int x, int n, int m) {
 
   result = notSwitchedBytes + (byteM ^ byteN); //2
   return result;
+  
+  return 2;
 }
+
+
 /* 
  * conditional - same as x ? y : z 
  *   Example: conditional(2,4,5) = 4
@@ -209,10 +262,27 @@ int byteSwap(int x, int n, int m) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-	return 2;
+  /*
+    Exploits ! to get either 0 or 1. Uses left shift/right 
+    shift to copy the LSB to every other bit (32 total).
+    
+    Uses & and ~ to get all 0's for one operation (either
+    p or ~p will be all 0's) and either y or z (depending on x)
+    for the other. Using | on the two results (one is all 0's)
+    will return the nonzero argument.
+   */
+  
+  int p = !x << 31 >> 31; // all 0's or all 1's
+  int q = p&z; // z if p is all 1's, 0 otherwise
+  int r = ~p&y; // y if p is all 0's, 0 otherwise
+  int result = q | r;
+
+  return result;
 }
+
+
 /* 
- * divpwr2 - Compute x/(2^n), for 0 <= n <= 30  
+ * divpwr2 - Compute x/(2^n), for 0 <= n <= 30
  *  Round toward zero
  *   Examples: divpwr2(15,1) = 7, divpwr2(-33,4) = -2
  *   Legal ops: ! ~ & ^ | + << >>
@@ -220,17 +290,31 @@ int conditional(int x, int y, int z) {
  *   Rating: 2
  */
 int divpwr2(int x, int n) {
-    int division = x >> n;
 
-    int sign = x >> 31;
-    int negN = ~n + 1;
-    int shift = 31 + negN;
-    int oneOnLeft = 0x1 << 31;
-    int check = ~(oneOnLeft >> shift);
+  /* Exploits fact that right shifting divides by 
+     powers of 2 correctly for positive values.
+     For negative values, checks divisibility
+     by checking the first n bits. Exploits 
+     ~ to subtract n from 31, left shifting
+     and right shifting to get 
+     1's for last n bits, then uses & to check
+     if any of the first n bits are nonzero.
+     Uses !! to give 0 if all are 0 and 1 if any
+     are nonzero. Using & with sign gives 1 if 
+     negative and not evenly divisible, 0 otherwise.
+     Then adds to the right shifted x value.
+   */
+ 
+  int sign = (x >> 31);
+  int div = x >> n; // evenly divisible?
+  int shift = 31 + (~n+1); // 31-n
+ 
+  int checkBit = ~((0x01 << 31) >> (shift));
+  
+  int addTo = sign & !!(x & checkBit);
+  int result = div + (addTo);
 
-    int add = !!(x & check) & sign;
-
-    return division + add;
+  return result;
 }
 /* 
  * evenBits - return word with all even-numbered bits set to 1
@@ -240,11 +324,16 @@ int divpwr2(int x, int n) {
  */
 int evenBits(void) {
   /*
-  Begins with the stored value 0x55. Shifts this left by 8 and ^s the result with the stored value to get 0x5555. Repeats the process (shifting by 16 this time) to result in 0x55555555 (0x([5] * 8)).
-  */
+    Exploits fact that return same value every time.
+
+    Starts with 0x55, shifts left 1 byte and exploits ^
+    to copy the byte. Repeats shifting by 2 bytes.
+   */
   int result = 0x55;
   result = result ^ (result << 8);
-  return result ^ (result << 16);
+  result = result ^ (result << 16);
+ 
+  return result;
 }
 /* 
  * float_abs - Return bit-level equivalent of absolute value of f for
@@ -280,6 +369,14 @@ unsigned float_i2f(int x) {
  *   Rating: 3
  */
 int isPositive(int x) {
+  /*
+    Exploits left shift to copy the sign bit.
+    Uses ! to differentiate between zero and nonzero.
+    Exploits ^ to make zero and negative the same case (all 1's)
+    and to make positive all 0's. 
+    
+    Exploits ! to make positive 1, negative and 0 become 0. 
+   */
   int X = (x >> 31);
   int Y = !x;
   int result = X ^ Y;
@@ -293,6 +390,11 @@ int isPositive(int x) {
  *   Rating: 1
  */
 int isZero(int x) {
+  /*
+    exploits fact that !0 returns a nonzero value (1), while 
+    ! any other value returns 0;
+   */
+  
   return !x;
 }
 /* 
@@ -302,12 +404,22 @@ int isZero(int x) {
  *   Rating: 1
  */
 int thirdBits(void) {
-  /**/
-  int firstGroup = 0x49; //0b01001001
-  int secondGroup = 0x2; //0b0010
-  int result = secondGroup ^ (firstGroup << 4); //0b010010010010
-  result = result ^ (result << 12); //0b010010010010010010010010
-  return firstGroup ^ (result << 8); //0b00100100100100100100100100100100
+  /*
+    Exploits the fact that the pattern repeats after 
+    12 bites. Exploits left shifts to make into groups of
+    12 bits, ^ puts parts together. 
+    Last byte is equal to the first byte, so
+    use that for last ^ operation. 
+   */
+
+  int firstGroup = 0x49;
+  int secondGroup = 0x2;
+  int result = (firstGroup << 4) ^ secondGroup;
+
+  result = (result << 12) ^ result;
+
+  result = (result << 8) ^ firstGroup;
+  return result;
 }
 /* 
  * TMax - return maximum two's complement integer 
@@ -316,9 +428,18 @@ int thirdBits(void) {
  *   Rating: 1
  */
 int tmax(void) {
+  /*
+    Exploits left shift to put 1 in sign bit, 
+    0's everywhere else.
+    Taking ~ switches sign to 0 and all others to 
+    1. 
+   */
   int result = 0x80;
+
   result = result << 24;
-  return ~result;
+  result = ~result;
+
+  return result;
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -327,6 +448,9 @@ int tmax(void) {
  *   Rating: 1
  */
 int tmin(void) {
-  int result = 0x80;
-  return result << 24;
+  /*
+    Exploits left shifting to move 1 to sign bit,
+    have 0's everywhere else.
+   */
+  return (0x80<<24);
 }
