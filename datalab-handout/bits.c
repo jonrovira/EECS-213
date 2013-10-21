801026039dfc6f8dc9b3207209216d6e187af834
curtiswiese <curtiswiese2016@u.northwestern.edu>
2013-10-21 02:03:12 -0500

    Done (need some comments)
    Got driver to work, removed unnecessary operations. Got down to 28 I
    think. You should comment these. Think bitParity and the two floats are
    the only ones who need it.

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -389,21 +389,36 @@ unsigned float_abs(unsigned uf) {
  *   Rating: 4
  */
 unsigned float_i2f(int x) {
+ 
   unsigned sign = x & 0x80000000;
+  unsigned checker = 0x40000000;
+  int magnitude = 31;
+  unsigned spot = 0;
+  int leftShift;
+  unsigned leftShifted;
+  unsigned twentyThreeBits;
+  unsigned twentyFifthBit;
+  unsigned addOne;
+  unsigned lastSeven;
+  unsigned result;
+  unsigned twentyFourthBit;
+  unsigned exponent;
+
+  
   if(x == 0x80000000) 
     return 0xcf000000;
   if(!!sign) {
     x = ~x + 1;
    }
-  unsigned checker = 0x40000000;
-  int magnitude = 31;
-  unsigned spot = 0;
+
+
+
 
   //Zero case
   if((x ^ 0x0) == 0x0) return 0x0;
 
   //Finds number by which to shift left
-  while((checker != 0x0) && (spot == 0)) {
+  while(spot == 0){    
     if((checker & x) == checker) {
       spot = magnitude;
     }
@@ -412,31 +427,30 @@ unsigned float_i2f(int x) {
   }
 
   // Shifts x left until the most significant 1 is on the left
-  int leftShift = 32 + (~spot + 1);
-  unsigned leftShifted = x << leftShift;
+  leftShift = 33 + (~spot);
+  leftShifted = x << leftShift;
 
-  // Twenty-one on far left, zeros on right
-  unsigned twentyThreeBits = leftShifted & 0x7fffff00;
+  // Twenty-three on far left, zeros on right
+  twentyThreeBits = leftShifted & 0x7fffff00;
 
   // Basic case to check if round up is even possibility
-  unsigned twentyFifthBit = leftShifted & 0x00000080;
-  unsigned addOne = 0;
-  if(twentyFifthBit == 0x80) {
-    unsigned lastSeven = leftShifted & 0x0000007f;
-    unsigned anyOnes = !lastSeven;
-    
-    
+  twentyFifthBit = leftShifted & 0x00000080;
+  addOne = 0;
+  
 
+  if(twentyFifthBit == 0x80) {
+    lastSeven = leftShifted & 0x0000007f;
+        
     // Round up
-    if(!anyOnes) {
+    if(lastSeven) {
       addOne = 1;
       // twentyThreeBits = ((twentyThreeBits >> 8) + 1) & 0x007FFFFF;
       //twentyThreeBits = twentyThreeBits + 0x00000100;
     }
     else {
-      unsigned twentyFourthBit = twentyThreeBits & 0x00000100;
+      twentyFourthBit = twentyThreeBits & 0x00000100;
       // Round up
-      if(!!twentyFourthBit) {
+      if(twentyFourthBit) {
 	//twentyThreeBits = ((twentyThreeBits >> 8) + 1) & 0x007FFFFF;
         addOne = 1;
       }
@@ -446,15 +460,15 @@ unsigned float_i2f(int x) {
   twentyThreeBits = twentyThreeBits >> 8;
 
   // Exponent
-  unsigned exponent = (spot + 0xFFFFFFFF) + 127;
+  exponent = (spot + 0xFFFFFFFF) + 127;
   exponent = exponent << 23;
 
   //twentyThreeBits = twentyThreeBits & 0x007FFFFF;
-  unsigned result =  twentyThreeBits + sign + exponent;
+  result =  twentyThreeBits + sign + exponent;
   if(addOne)
     result = result + 1;
   return result;
-
+ 
 
 }
 /* 
